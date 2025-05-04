d829539a03484b32b08cc3f2a10a7cdfcfedf732
Amitkumarsharma7082 <aksharma7404@gmail.com>
2025-05-05 00:43:01 +0530

    install url packages and urls

--- 2-server/index.js
+++ 2-server/index.js
@@ -4,18 +4,32 @@ const http = require("http");
 //! 4. File system
 const fs = require("fs");
 
+//! 5. Url
+const url = require("url");
+
 //! 2. create a server
 const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
+  const myUrl = url.parse(req.url, true); // true query parameter
+  // http://localhost:8000/about?myName=Amit&userId=1
+  console.log(myUrl);
   fs.appendFile(
     "./log.txt",
     `${Date.now()} : ${req.url} : New Request\n`,
     (err, result) => {
-      switch (req.url) {
+      switch (myUrl.pathname) {
         case "/":
           res.end("HomePage");
           break;
         case "/about":
-          res.end("I am Amit");
+          // http://localhost:8000/about?myName=Amit
+          const userName = myUrl.query.myName;
+          res.end(`Hi ${userName}`);
+          break;
+        case "/users":
+          // http://localhost:8000/users?users=Amit
+          const users = myUrl.query.users;
+          res.end(`Hello from users : ${users}`);
           break;
         default:
           res.end("404 PAGE NOT FOUND");
